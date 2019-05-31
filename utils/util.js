b881c55e1a57803e17d327d3f966f2318e316232
liuhw <liuhuansir@126.com>
2019-05-31 09:10:08 +0800

    获取客户端的IP

--- utils/util.js
+++ utils/util.js
@@ -1,4 +1,4 @@
-exports.getClientIP = function getClientIP(req) {
+exports.getClientIP = function (req) {
   let clientIp = req.headers['x-forwarded-for'] || // 判断是否有反向代理 IP
     req.connection.remoteAddress || // 判断 connection 的远程 IP
     req.socket.remoteAddress || // 判断后端的 socket 的 IP
